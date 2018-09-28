749cde5601ff07201c7b0982f49c50b3b4d267aa
Edgar Martinez Flores <emf080996@gmail.com>
2018-09-28 03:06:55 -0500

    se agrega uso de memoria dinamica

--- tiendaE.cpp
+++ tiendaE.cpp
@@ -1,160 +1,115 @@
 #include <iostream>
 #include <conio.h>
-#include <iomanip>
-
+#include <stdlib.h>
 using namespace std;
-
-const int num=5;
-struct infoProducto{
-	string caducidad;
-	char tamano[20];
-	int cantidad;
+//estructura tienda 
+struct Tienda{
+	char clave[20];//clave del producto 
+	char nombre[25];//nombre del producto
+	int cantidad;//cantidad de producto
+	int precio;//precio del producto
 };
 
-struct producto{
-	int clave;
-	char nombre[15];
-	struct infoProducto datosProducto;
-	double precio;
-	double totalVendido;
-}productos[num];
+// declarando nodo
+struct Nodo{
+	Tienda producto;//producto variable para la estructura tienda
+	Nodo *siguiente;//Nodo tipo puntero siguiente
+};
 
 void menu();
-void agregarProducto();
-void bodega();
-int buscarLugarVacio();
-void venderProducto(int);
-int buscarClave(int);
+void agregarProducto(Tienda &);
+void insertarProducto(Nodo *&,Nodo *&,Tienda);
+bool vacio(Nodo *);
+void mostrarProductos(Nodo *&,Nodo *&,Tienda &);
+void venta(Nodo *&,int &);
 
-int main()
-{
-	int opcion;
-	int clave;
+int main(){
+	Nodo *frente=NULL;//nodo que apuntara siempre al frente de la cola
+	Nodo *fin=NULL;//nodo que apuntara a el final de la cola
+	Nodo *aux=NULL;//auxiliar para imprimir datos sin alterar a nodo frente
+	Tienda producto;
+	char opcion;//variable para el switch
+	int vender;//almacenara la clave del producto que se desea vender
+	double caja=0;
+	
 	do{
 		menu();
 		cin>>opcion;
-		
-		switch(opcion)
-		{
-			case 1:
-				agregarProducto();
-				break;
-			case 2:
-				bodega();
-				break;
-			case 3:
-				bodega();
-				cout<<"\nIntroducir la clave del producto a vender: "; cin>>clave;
-				venderProducto(clave);
-				break;
-			case 4:
-				cout<<"\nGracias por utulizar el programa."<<endl;
-				break;
-			default:
-				cout<<"Opcion no valida";
-				break;
-		}
-		getch();
-		system("cls");
-			
-	}while(opcion != 4);
-	return 0;
+		fflush(stdin);//vacia el buffer
+    	switch(opcion){
+	    	case '1':
+		    	agregarProducto(producto);//llamando a funcion para agregar productos
+		    	
+		    	insertarProducto(frente,fin,producto);//llamando funcion para insertar los nodos a la cola
+			    break;
+    		case '2':
+    			/*cout<<"Introducir la clave del elemento a vender: "; cin>>vender;
+	    		venta(producto,vender);*/
+		    	break;
+    		case '3':
+    			cout<<"Mostrando productos."<<endl;
+    			aux=frente;//nodo aux toma valor de frente para poder usar todos los datos de elnodo frente
+    			cout<<"CLAVE"<<"\tNOMBRE"<<"\t\t\tPRECIO"<<"\t\tCANTIDAD"<<endl;
+	    		while(aux !=NULL){
+	    			cout<<aux->producto.clave<<"\t"<<aux->producto.nombre<<"\t\t\t"<<aux->producto.precio++<<"\t\t"<<aux->producto.cantidad<<endl;
+		  	     	aux=aux->siguiente;//operacion para recorrer los nodos 
+				}
+				cout<<"Dinero en caja: "<<caja;//imprime dinero en caja
+		    	break;
+   	        case '4':
+   	        	cout<<"Salio del programa..."<<endl;
+   	        	break;
+      	    default:
+      	    	cout<<"\nOpcion invalida."<<endl;
+      	    	break;
+    	}
+    	getch();//pausa para evitar que se limpie pantalla sin que el usuario la vea
+    	system("cls");//Limpia pantalla
+    }while(opcion !='4');
+	return 0;//retorna un 0 si no hay errores
 }
 
+//funcion que muestra menu al usuario
 void menu(){
-	cout<<"Bienvenido "<<endl;
-	cout<<"1.Agregar productos"<<endl;
-	cout<<"2.Ver productos disponibles"<<endl;
-	cout<<"3.Vender producto"<<endl;
-	cout<<"4. Salir"<<endl;
-	cout<<"Ingresar la operacion que desea realizar: ";
+	cout<<"\nBienvenido"<<endl;
+	cout<<"1.Ingresar producto."<<endl
+	    <<"2.Vender producto."<<endl
+	    <<"3.Mostrar productos."<<endl
+	    <<"4.Salir"<<endl
+	    <<"Ingresar una opcion del menu: ";
 }
 
-void bodega()
-{
-	cout << endl;
-    cout << setw(6) << "CLAVE"
-            << setw(35) << "NOMBRE"
-            << setw(25) << "TAMANO"
-            << setw(15) << "CADUCIDAD"
-            << setw(25) << "CANTIDAD"
-            << setw(7) << "PRECIO" << endl;
-    for (int i = 0; i < num; i++) {
-        if (productos[i].clave != 0) {
-            cout << setw(6) << productos[i].clave
-                    << setw(35) << productos[i].nombre
-                    << setw(25) << productos[i].datosProducto.tamano
-                    << setw(15) << productos[i].datosProducto.caducidad
-                    << setw(25) << productos[i].datosProducto.cantidad
-                    << setw(7) << productos[i].precio<<"$"<<endl;
-        }
-    }
-    cout<<"\nDinero en caja: "<<productos[0].totalVendido<<"$"<<endl;
+//Pide datos del producto 
+void agregarProducto(Tienda &producto){
+	cout<<"Ingresar clave del producto: "; cin.getline(producto.clave,20,'\n');
+    cout<<"\nIngresar nombre del producto: "; cin.getline(producto.nombre,25,'\n');
+   	cout<<"\nIngresar precio del producto: "; cin>>producto.precio;
+   	cout<<"\nIngresar cantidad del producto: "; cin>>producto.cantidad;
 }
 
-void agregarProducto(){
-	int lugarVacio;
-	lugarVacio=buscarLugarVacio();
-	if(lugarVacio==-1){
-		cout<<"\nYa no hay lugar disponible. "<<endl;
-		return;
+//Inserta los nodos y agrega un nuevo nodo
+void insertarProducto(Nodo *&frente,Nodo *&fin,Tienda producto){
+	Nodo *nuevo_nodo = new Nodo();//creando nodo
+	nuevo_nodo->producto=producto;//insertando la estructura al nuevo nodo 
+	nuevo_nodo->siguiente=NULL; //el nodo siguiente se iguala a null no contiene valor
+	
+	if(vacio(frente)){
+		frente = nuevo_nodo;//si la cola esta vacia frente sera = al nuevo nodo creado
+	}else{
+		fin->siguiente = nuevo_nodo;//si hay mas de un nodo se agregara el nodo siguiente
 	}
-	fflush(stdin);//borra los elementos basura de la siguiente variable
-	cout<<"\nIngresar clave del producto: "; cin>>productos[lugarVacio].clave;
-	fflush(stdin);
-	cout<<"\nIngresar nombre del producto: "; cin.getline(productos[lugarVacio].nombre,15,'\n');
-	cout<<"\nIngresar precio del producto: "; cin>>productos[lugarVacio].precio;
-	fflush(stdin);
-	cout<<"\nIngresar cantidad del producto: "; cin>>productos[lugarVacio].datosProducto.cantidad;
-	fflush(stdin);
-	cout<<"\nIngresar tamano del producto: "; cin.getline(productos[lugarVacio].datosProducto.tamano,20,'\n');
-	cout<<"\nIngresar caducidad del producto: "; getline(cin,productos[lugarVacio].datosProducto.caducidad);
-	cout<<"\n";
-	
+	fin = nuevo_nodo;//el nuevo nodo introducido siempre sera el nodo final
 }
 
-int buscarLugarVacio(){
-	
-	for(int i=0; i<num; i++)
-	{
-		if(productos[i].clave == 0)
-		{
-			return i;
-		}
-	}
-	return -1;
-	
+//retorna true si la cola esta vacia o false si hay mas nodos
+bool vacio(Nodo *frente){
+	return (frente==NULL)? true : false;//si frente = a NULL no hay nodos introducidos 
 }
 
-void venderProducto(int clavex){
-	int cantidadx;
-	int claveR=buscarClave(clavex);
-	double total;
+/*void venta(Nodo *&frente,int &n){
 	
-	if(claveR !=-1&&productos[claveR].datosProducto.cantidad !=0)
-	{
-		cout<<"Ingresar cuantos productos desea vender: "; cin>>cantidadx;
+	if(buscar(n)){
 		
-		productos[claveR].datosProducto.cantidad=productos[claveR].datosProducto.cantidad-cantidadx;
-		total=productos[claveR].precio*cantidadx;
-		productos[0].totalVendido=productos[0].totalVendido+total;
-		cout<<"<<<<<<\nProducto vendido>>>>>> \n"<<endl;
-		bodega();
-		return;
-	}else if(productos[claveR].datosProducto.cantidad==0){
-		cout<<"Producto no disponible"<<endl;
-		return;
 	}
 	
-}
-
-int buscarClave(int clavex){
-	for(int i=0; i<num; i++)
-	{
-		if(clavex==productos[i].clave){
-			return i;
-		}
-	}
-	cout<<"Clave no encontrada "<<endl;
-	return -1;
-}
+}*/
